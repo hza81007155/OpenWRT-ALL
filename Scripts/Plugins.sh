93a18015aa1ee28a289c1fedfe444ea721e92858
Rolay <rolay8@gmail.com>
2023-07-29 21:24:52 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -12,7 +12,8 @@ git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 
 # packages
-git clone --depth=1 --single-branch https://github.com/kiddin9/openwrt-packages.git
+# git clone --depth=1 --single-branch https://github.com/kiddin9/openwrt-packages.git
+
 #Open Clash
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 #Pass Wall
