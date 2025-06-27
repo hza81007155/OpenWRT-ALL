f3b67467997a5d506e45bfa872596d58fb6cbf34
hza81007155 <800755@qq.com>
2025-06-27 16:25:21 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -12,8 +12,7 @@ git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luc
 #git clone --depth=1 --single-branch https://github.com/kiddin9/openwrt-packages.git kiddin9
 
 # 18.06 Argon theme
-git_clone 18.06 https://github.com/jerrykuku/luci-theme-argon
-git_clone 18.06 https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/kenzok78/luci-theme-argone
 
 # dockerman
 git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-dockerman.git
